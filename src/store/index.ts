1545290084446a229dbc8f225a464131b054a1d7
Alexander Bukin <lxndrbukin@proton.me>
2026-03-06 08:44:44 +0000

    Refactor language slice into system slice for future handling of entire system state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import language from "./slices/languageSlice";
+import system from "./slices/systemSlice";
+import catalog from "./slices/catalogSlice";
 
 export const store = configureStore({
-  reducer: { language },
+  reducer: {
+    system,
+    catalog,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export * from "./slices/types";
-export * from "./slices/languageSlice";
+export * from "./slices/systemSlice";
+export * from "./slices/catalogSlice";
+export * from "./thunks/items";
