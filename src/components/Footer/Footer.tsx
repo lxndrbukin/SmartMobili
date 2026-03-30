35e00a457bccc873e0709e230e2c938e78c22030
Alexander Bukin <lxndrbukin@proton.me>
2026-03-30 07:14:33 +0100

    Update footer locale and links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,23 +1,25 @@
 import { type JSX } from "react";
+import { Link } from "react-router-dom";
+import useLocalePath from "../../hooks/useLocalePath";
 import { useTranslation } from "react-i18next";
 import type { FooterTranslations, NavLink } from "./types";
 
 export default function Footer(): JSX.Element {
   const { t } = useTranslation("footer");
-
+  const to = useLocalePath();
   const sections = t("sections", { returnObjects: true }) as FooterTranslations;
 
   const renderSectionLinks = (links: Array<NavLink>) => {
     return links.map((link: NavLink) => {
       return (
         <li>
-          <a href={link.href}>{link.label}</a>
+          <Link to={to(`/catalog${link.href}`)}>{link.label}</Link>
         </li>
       );
     });
   };
 
-  const renderSections = () => {
+  const renderSections = (sections: FooterTranslations) => {
     return Object.values(sections).map((section) => {
       return (
         <div className="footer-col">
@@ -32,19 +34,22 @@ export default function Footer(): JSX.Element {
     <div className="footer-wrapper">
       <footer className="footer">
         <div className="footer-col">
-          <a className="logo logo-dark" href="/">
+          <Link className="logo logo-dark" to={to("/")}>
             <div className="logo_primary">
               <span className="logo_primary-left">SMART</span>
               <span className="logo_primary-right">MOBILI</span>
             </div>
-          </a>
+          </Link>
           <div className="footer-socials">
-            <i className="fab fa-instagram-square"></i>
-            <i className="fab fa-telegram-plane"></i>
-            <i className="fab fa-facebook-square"></i>
+            <a target="_blank" href="https://www.instagram.com/sm_smartmobili/">
+              <i className="fab fa-instagram-square"></i>
+            </a>
+            <a target="_blank" href="https://t.me/SM_smartmobili">
+              <i className="fab fa-telegram-plane"></i>
+            </a>
           </div>
         </div>
-        {renderSections()}
+        {renderSections(sections)}
       </footer>
     </div>
   );
