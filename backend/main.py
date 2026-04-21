797e7fa41d428280dbd24f0e433d378af5c387c6
Alexander Bukin <lxndrbukin@proton.me>
2026-04-21 12:56:42 +0100

    Update CORS

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app.include_router(v1_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://smartmobili-xi.vercel.app"],
+    allow_origins=["http://localhost:5173", "https://smartmobili.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
