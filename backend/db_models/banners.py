162cacf331dcde2bede30a54cb3603fa2a8e2b3d
lxndrbukin <lxndrbukin@proton.me>
2026-08-27 07:36:12 +0100

    Work on models for future banner creation through admin panel

---
+++ backend/db_models/banners.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from db import Base
+
+class Banner(Base):
+    __tablename__ = 'banners'
+
+    id = Column(Integer, primary_key=True, index=True)
+    url = Column(String(100), nullable=False)
+    
+    images = relationship('BannerImage', back_populates='banner', cascade='all, delete-orphan')
+    translations = relationship('BannerTranslation', back_populates='banner', cascade='all, delete-orphan')
+
+class BannerTranslation(Base):
+    __tablename__ = 'banner_translation'
+
+    id = Column(Integer, primary_key=True, index=True)
+    banner_id = Column(Integer, ForeignKey('banners.id'))
+    language = Column(String(2), nullable=False)
+    header = Column(String(50), nullable=False)
+    body = Column(String(200), nullable=True)
+
+    banner = relationship('Banner', back_populates='translations')
+
+class BannerImage(Base):
+    __tablename__ = 'banner_images'
+
+    id = Column(Integer, primary_key=True, index=True)
+    banner_id  = Column(Integer, ForeignKey('banners.id'))
+    image_url = Column(String(500), nullable=False)
+    order = Column(Integer, default=0)
+
+    banner = relationship('Banner', back_populates='images')
+
---
+++ backend/models/banners.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel, field_validator
+from utils import Language
+
+class BannerImageResponse(BaseModel):
+    id: int
+    image_url: str
+    order: int
+
+    class Config:
+        from_attributes = True
+
+class BannerTranslationCreate(BaseModel):
+    language: Language
+    header: str
+    body: str | None = None
+
+class BannerTranslationUpdate(BaseModel):
+    header: str
+    body: str | None = None
+
+class BannerTranslationResponse(BaseModel):
+    id: int
+    language: Language
+    header: str
+    body: str | None = None
+
+class BannerCreate(BaseModel):
+    url: str
+    translations: list[BannerTranslationCreate]
+
+    @field_validator("translations")
+    def romanian_required(cls, translations):
+        languages = [t.language for t in translations]
+        if Language.ro not in languages:
+            raise ValueError("Romanian (ro) text is required")
+        return translations
+
+class BannerUpdate(BaseModel):
+    url: str
+
+class BannerResponse(BaseModel):
+    id: int
+    url: str
+    header: str
+    body: str | None
+    language: Language
+    images: list[BannerImageResponse] = []
